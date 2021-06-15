dff3e97791807234d8ba0fdb1e05717fc7c21473
Sebastian Rollen <seb@overmu.se>
2021-06-15 13:43:27 -0400

    fix data output format

--- src/DoubleTrouble.jl
+++ src/DoubleTrouble.jl
@@ -26,8 +26,9 @@ function main()
     shortterm_spreads = spreads(data[end-30*79:end, :])
     longterm_data = data[reduce(vcat, [[79*(j-1)+1, 79j] for j in 1:100]), :]
     longterm_spreads = spreads(longterm_data)
-    initial_prices = data[1, :]
-    pairs_data = generate_trade_pairs(tickers, shortterm_spreads, longterm_spreads, initial_prices)
+    initial_lt_prices = data[1, :]
+    initial_st_prices = data[end-30*79, :]
+    pairs_data = generate_trade_pairs(tickers, shortterm_spreads, longterm_spreads, initial_lt_prices, initial_st_prices)
     CSV.write(ENV["OUT_FILE"], DataFrame(pairs_data))
 end
 
